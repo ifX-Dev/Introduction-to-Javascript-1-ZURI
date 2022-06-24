f62c25a3ddc1de9b932280f64e5cffd5042ab69a
Ifeoluwapo Thomas <91812312+sincerelydany@users.noreply.github.com>
2022-06-24 19:11:12 +0100

    Update intro.js

--- intro.js
+++ intro.js
@@ -2,6 +2,6 @@ var my_name = "Ifeoluwa Thomas"
 var height = "163"
 var country = "Nigeria"
 
-document.write("My name is " + my_name + ".<br>")
-document.write("I am " + height + " centimeters tall.<br>")
-document.write("I am from " + country + ".")
+document.write("My name is " + my_name + ".<br>");
+document.write("I am " + height + " centimeters tall.<br>");
+document.write("I am from " + country + ".");
